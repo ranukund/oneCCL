10e0e57d0b64f8a50cce43e57b08d161c635e24a
Nikita Gusev <nikita.gusev@intel.com>
2025-05-06 20:38:57 +0200

    Intel(R) oneAPI Collective Communications Library (oneCCL) 2021.15.1 (#167)
    * Intel(R) oneAPI Collective Communications Library (oneCCL) 2021.15.1

--- src/common/api_wrapper/ofi_api_wrapper.cpp
+++ src/common/api_wrapper/ofi_api_wrapper.cpp
@@ -13,9 +13,12 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
+#include <dlfcn.h>
+#include <libgen.h>
 #include <sys/stat.h>
 
 #include "common/api_wrapper/api_wrapper.hpp"
+#include "common/log/log.hpp"
 #include "common/api_wrapper/ofi_api_wrapper.hpp"
 
 namespace ccl {
@@ -64,20 +67,53 @@ std::string get_ofi_lib_path() {
     return ofi_lib_path;
 }
 
-bool ofi_api_init() {
-    bool ret = true;
+std::string get_relative_ofi_lib_path() {
+    Dl_info info;
 
+    if (dladdr((void*)ccl::get_library_version, &info)) {
+        char libccl_path[PATH_MAX];
+
+        if (realpath(info.dli_fname, libccl_path) != nullptr) {
+            // We have to use `realpath`, so the dirname will work correctly,
+            // because if there's any symlink like `..` in the path it will not work
+            libccl_path[PATH_MAX - 1] = '\0';
+
+            // Remove `libccl.so` from the path to get directory like `$CCL_ROOT/lib`
+            char* libccl_dir = dirname(libccl_path);
+            // Remove the `lib` from the path the get just the `CCL_ROOT`
+            char* ccl_root_cstr = dirname(libccl_dir);
+
+            auto ccl_root = std::string(ccl_root_cstr);
+            setenv("FI_PROVIDER_PATH", (ccl_root + "/lib/libfabric/prov").c_str(), 0);
+            return ccl_root + "/lib/libfabric/libfabric.so";
+        }
+    }
+
+    LOG_DEBUG("Could not fetch relative path to libfabric. Fallback to `libfabric.so`");
+    return "libfabric.so";
+}
+
+bool ofi_api_init() {
     ofi_lib_info.ops = &ofi_lib_ops;
     ofi_lib_info.fn_names = ofi_fn_names;
     ofi_lib_info.path = get_ofi_lib_path();
 
     int error = load_library(ofi_lib_info);
-    if (error != CCL_LOAD_LB_SUCCESS) {
-        print_error(error, ofi_lib_info);
-        ret = false;
+    if (error == CCL_LOAD_LB_SUCCESS) {
+        return true;
     }
 
-    return ret;
+    print_error(error, ofi_lib_info);
+    LOG_INFO("Retrying to load libfabric.so using relative path");
+
+    ofi_lib_info.path = get_relative_ofi_lib_path();
+    error = load_library(ofi_lib_info);
+    if (error == CCL_LOAD_LB_SUCCESS) {
+        return true;
+    }
+
+    print_error(error, ofi_lib_info);
+    return false;
 }
 
 void ofi_api_fini() {
